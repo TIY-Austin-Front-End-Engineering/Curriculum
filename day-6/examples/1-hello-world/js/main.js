9230f493ebacced807c61bc7794f2cc44920195a
Aaron Larner <anlarner@gmail.com>
2015-01-13 22:33:39 -0600

    Add beginner javascript examples

---
+++ day-6/examples/1-hello-world/js/main.js
@@ -0,0 +1,102 @@
+// console.log('hello world!');
+// console.log('this is javascript!');
+// console.log(5);
+// console.log(5.7);
+// console.log(-3.14);
+// console.log(true);
+// console.log(false);
+
+// var myFirstName = 'Aaron';
+
+// console.log(myFirstName);
+
+// myFirstName = 'Luke';
+
+// console.log(myFirstName);
+
+
+// var myFirstName = 'Aaron';		
+// var myLastName = 'Larner';
+// var temp;
+
+// temp = myFirstName;
+
+// myFirstName = myLastName;
+
+// myLastName = temp;
+
+
+// console.log(myFirstName);
+// console.log(myLastName);
+
+
+// console.log('10');
+// console.log('abc');
+// console.log(10);
+// console.log('10'-0)
+
+// console.log(parseInt('10'));
+// console.log(parseInt('10.123'));
+// console.log(parseFloat('sdfjhskd'));
+
+
+// console.log('10'+1);
+// console.log(parseInt('10')+1);
+
+// console.log('10*3');
+// console.log(parseFloat('10.7*3'));
+
+// console.log(true && true);
+// console.log(true && false);
+
+// console.log(true || false);
+// console.log(true || true);
+// console.log(false || false);
+
+// American flag example
+// var blue = true;
+// var red = true;
+
+
+// console.log(blue && red);
+
+// // Swiss flag example
+// var blue = true;
+// var red = false;
+
+// console.log(blue && red);
+// console.log(blue || red);
+
+
+// console.log(0);
+// console.log(false);
+
+// parseInt('123');
+// Boolean(0);
+// Boolean('abc');
+// Boolean('');
+
+// console.log('thi' == false);
+
+var myName = 'Bob';
+
+
+
+myName = 'Aaron';
+
+// if(myName === 'Aaron') {
+// 	console.log('Hey '+myName+', how\'s it going?');
+// }
+
+// if(myName !== 'Aaron') {
+// 	console.log('Get off the podium '+myName);
+// }
+
+
+
+
+
+
+
+
+
