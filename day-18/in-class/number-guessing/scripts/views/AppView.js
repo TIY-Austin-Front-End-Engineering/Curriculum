293f678787f94d0adbc891a3223ed938453831d2
Aaron Larner <anlarner@gmail.com>
2015-02-02 16:51:32 -0600

    Backbone example

---
+++ day-18/in-class/number-guessing/scripts/views/AppView.js
@@ -0,0 +1,12 @@
+var AppView = Backbone.View.extend({
+	el: '#app-view',
+	initialize: function() {
+		console.log('Initialize AppView');
+
+		var jquery_random = $('#random');
+
+		// console.log(jquery_random[0]);
+
+		// jquery_random.html(Math.random());
+	}
+});
